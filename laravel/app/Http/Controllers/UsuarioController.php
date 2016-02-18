9eacb92773b6dd7441d2bc78f5a690ea24cbc370
gamaliellobato <gamaliellobato@gmail.com>
2016-02-18 16:29:59 -0600

    13.Validaciones

--- laravel/app/Http/Controllers/UsuarioController.php
+++ laravel/app/Http/Controllers/UsuarioController.php
@@ -1,106 +1,96 @@
-<?php namespace Cinema\Http\Controllers;
-
+<?php
+namespace Cinema\Http\Controllers;
+use Illuminate\Http\Request;
 use Cinema\Http\Requests;
+use Cinema\Http\Requests\UserCreateRequest;
+use Cinema\Http\Requests\UserUpdateRequest;
 use Cinema\Http\Controllers\Controller;
+use Cinema\User;
 use Session;
 use Redirect;
-use Cinema\User;
-
-use Illuminate\Http\Request;
-
-class UsuarioController extends Controller {
-
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-		$users = User::all();
+class UsuarioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::all();
         return view('usuario.index',compact('users'));
-	}
-
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		return view('usuario.create');
-	}
-
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store(Request $request)
-	{
-		User::create([
-			'name' => $request['name'],
-			'email' => $request['email'],
-			'password' => $request['pasword'],
-			]);
-
-		return redirect('/usuario')->with('message','store');
-	}
-
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		//
-	}
-
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		//
-		$user = User::find($id);
-		return view('usuario.edit',['user'=>$user]);
-	}
-
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id, Request $request)
-	{
-		//
-		$user = User::find($id);
-		$user->fill($request->all());
-		$user->save();
-
-		Session::flash('message',"Usuario Editado Correctamente");
-		return Redirect::to('/usuario');
-	}
-
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
-		User::destroy($id);
-		Session::flash('message',"Usuario Eliminado Correctamente");
-		return Redirect::to('/usuario');
-
-	}
-
+    }
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('usuario.create');
+    }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(UserCreateRequest $request)
+    {
+        User::create([
+            'name' => $request['name'],
+            'email' => $request['email'],
+            'password' => $request['password'],
+        ]);
+        
+        return redirect('/usuario')->with('message','store');
+    }
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $user = User::find($id);
+        return view('usuario.edit',['user'=>$user]);
+    }
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UserUpdateRequest $request, $id)
+    {
+        $user = User::find($id);
+        $user->fill($request->all());
+        $user->save();
+        Session::flash('message','Usuario Actualizado Correctamente');
+        return Redirect::to('/usuario');
+    }
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        User::destroy($id);
+        Session::flash('message','Usuario Eliminado Correctamente');
+        return Redirect::to('/usuario');
+    }
 }
--- laravel/resources/views/usuario/create.blade.php
+++ laravel/resources/views/usuario/create.blade.php
@@ -1,10 +1,8 @@
 @extends('layouts.admin')
-
-@section('content')
-
+	@section('content')
+	@include('alerts.request')
 	{!!Form::open(['route'=>'usuario.store', 'method'=>'POST'])!!}
 		@include('usuario.forms.usr')
-	{!!Form::submit('Registrar',['class'=>'btn btn-primary'])!!}
+		{!!Form::submit('Registrar',['class'=>'btn btn-primary'])!!}
 	{!!Form::close()!!}
-
-@stop
+	@endsection
