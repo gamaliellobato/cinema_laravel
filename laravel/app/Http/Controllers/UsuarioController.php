5cb5ab59272694178136fc750cc088bc189d7841
gamaliellobato <gamaliellobato@gmail.com>
2016-02-18 15:42:08 -0600

    12.CRUD Eliminar usuario

--- laravel/app/Http/Controllers/UsuarioController.php
+++ laravel/app/Http/Controllers/UsuarioController.php
@@ -97,6 +97,10 @@ class UsuarioController extends Controller {
 	public function destroy($id)
 	{
 		//
+		User::destroy($id);
+		Session::flash('message',"Usuario Eliminado Correctamente");
+		return Redirect::to('/usuario');
+
 	}
 
 }
--- laravel/resources/views/usuario/edit.blade.php
+++ laravel/resources/views/usuario/edit.blade.php
@@ -2,6 +2,11 @@
 @section('content')
 {!!Form::model($user,['route'=> ['usuario.update',$user->id],'method'=>'PUT'])!!}
 	@include('usuario.forms.usr')
-	{!!Form::submit('Registrar',['class'=>'btn btn-primary'])!!}
+	{!!Form::submit('Actualizar',['class'=>'btn btn-primary'])!!}
+	{!!Form::close()!!}
+
+	{!!Form::open(['route'=> ['usuario.destroy',$user->id],'method'=>'DELETE'])!!}
+	
+	{!!Form::submit('Eliminar',['class'=>'btn btn-danger'])!!}
 	{!!Form::close()!!}
 @stop
